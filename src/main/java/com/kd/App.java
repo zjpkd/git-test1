3864815fb6e00504dcb674395592a655e4fee56b
zjp <zjp@qq.com>
2022-04-17 10:41:03 +0800

    hot-fix commit2

--- src/main/java/com/kd/App.java
+++ src/main/java/com/kd/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "Hello git!" );
         System.out.println( "Hello git1!" );
+        System.out.println( "hot-fix test!" );
     }
 }
